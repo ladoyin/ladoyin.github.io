8089cfed5ea82b3ff3e64e7dd4c4946ebc1b42f8
Ojo Oladoyin <ojooladoyin27@yahoo.com>
2018-06-28 11:09:02 +0100

    modified sw

--- javascript/sw.js
+++ javascript/sw.js
@@ -1,15 +1,16 @@
-var cacheName = 'converter-v2';
-
+var cacheName = 'converter-v1';
+var urlsToCache = [
+    './',
+    './css/convert.css',
+    './javascript/convert.js',
+    './index.html'
+];
 self.addEventListener('install', function(event){
+    console.log('service worker installing');
     event.waitUntil(
         caches.open(cacheName).then(function(cache) {
             console.log(cache, 'opened cache');
-            return cache.addAll([
-                './',
-                './css/convert.css',
-                './javascript/convert.js',
-                './index.html'
-            ]);
+            return cache.addAll(urlsToCache);
         })
     );
 });
