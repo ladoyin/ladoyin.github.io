7d1c48fa68660fab7c5efde87d84b97cc0fe0839
Ojo Oladoyin <ojooladoyin27@yahoo.com>
2018-06-28 19:59:46 +0100

    idb4

--- javascript/convert.js
+++ javascript/convert.js
@@ -13,7 +13,7 @@ function openDatabase(){
     const apiUrl = 'https://free.currencyconverterapi.com/api/v5/countries';
     let currenciesOfCountries;
     let dbPromise = idb.open('currncies-country', 1, upgradeDb =>{
-        let store = upgradeDb.createObjectStore('currencies', {keyPath: 'id'});
+        let store = upgradeDb.createObjectStore('currencies', {keyPath: 'currencyName'});
         store.createIndex('curreny-Id', 'currencyId');
     });
     fetch(apiUrl).then(response =>{
