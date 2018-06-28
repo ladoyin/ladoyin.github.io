bdb17dba9672c840f831c2406c9d429de952a207
Ojo Oladoyin <ojooladoyin27@yahoo.com>
2018-06-28 10:22:00 +0100

    modified the service worker registration

--- javascript/convert.js
+++ javascript/convert.js
@@ -1,7 +1,11 @@
-if('ServiceWorker' in navigator){
-    navigator.serviceWorker.register('/javascript/sw.js')
-    .then(registration => console.log('registration successful, scope is:', registration.scope))
-    .catch(error => console.log('Service worker registration failed, error:', error));
+if('serviceWorker' in navigator){
+    window.addEventListener('load', function(){
+        navigator.serviceWorker.register('/javascript/sw.js').then(function (registration){
+        console.log('registration successful, scope is:', registration);
+    }).catch(function (error){
+        console.log('Service worker registration failed, error:', error);
+    });
+    });
 }
 
 
